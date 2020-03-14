a1b6e6e8fccfd933511338544cc043bd6a9f802e
Anhgg0805 <anhsowon@gmail.com>
2020-03-14 16:07:45 +0700

    4 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { SlideComponent } from './slide/slide.component';
+import { ProduceListComponent } from './produce-list/produce-list.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, SlideComponent, ProduceListComponent, ProductComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/produce-list/produce-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produce-list',
+  templateUrl: './produce-list.component.html',
+  styleUrls: ['./produce-list.component.css']
+})
+export class ProduceListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
